798332aaeff96b9facb73d1e93e6794bc9b44831
Bryan Octavio <100432689@alumnos.uc3m.es>
2023-03-20 19:02:09 +0100

    feat: header comp

--- app/index.js
+++ app/index.js
@@ -1,9 +1,59 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, SIZES, icons, images } from '../constants';
+import {
+  Popularjobs,
+  Nearbyjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function Home() {
+  const handlePress = (e) => {};
   return (
-    <View>
-      <Text>Aplicación de trabajos</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: SIZES.medium,
+        backgroundColor: COLORS.gray2,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.gray,
+          },
+          headerTitle: '',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension='60%'
+              handlePress={handlePress}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={images.profile}
+              dimension='100%'
+              handlePress={handlePress}
+            />
+          ),
+        }}
+      />
+      <ScrollView
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.lightWhite,
+        }}
+      >
+        <Welcome />
+        <Popularjobs />
+        <Nearbyjobs />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
