c711cba0106479d8974ed663f8c5218ed367cd74
Bryan Octavio <100432689@alumnos.uc3m.es>
2023-03-23 12:08:00 +0100

    add: deatils page layout

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -6,6 +6,8 @@ import {
   ScrollView,
   ActivityIndicator,
   RefreshControl,
+  SafeAreaView,
+  Share,
 } from "react-native";
 import { localeJobsMock } from "../../mocks/jobs";
 import {
@@ -19,14 +21,82 @@ import {
 import { COLORS, SIZES, icons } from "../../constants";
 import { useFetch } from "../../hooks/useFetch";
 function Id() {
+  const [refreshing, setRefreshing] = useState(false);
   const router = useRouter();
   const params = useSearchParams();
   // const {data,isLoading, error,refetch} = useFetch
+  const isLoading = false;
+  const error = false;
   const job = localeJobsMock.find((item) => item.job_id === params.id);
+  const data = [job];
+  const handleRefresh = () => {};
+
+  const handleShare = async (jobLink) => {
+    try {
+      await Share.share({
+        message: `Look at this job ${jobLink}`,
+        url: jobLink,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   return (
-    <View>
-      <Text>{job.job_title}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerTitle: "",
+          headerBackVisible: false,
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension="60%"
+              handlePress={() => handleShare(data[0].job_apply_link)}
+            />
+          ),
+        }}
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View
+              style={{
+                padding: SIZES.medium,
+                paddingBottom: 100,
+              }}
+            >
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
   );
 }
 
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { checkImageURL } from "../../../utils/checkImage";
 
-import styles from './company.style'
+import styles from "./company.style";
 
-const Company = () => {
+const Company = ({ companyLogo, companyName, jobTitle, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+          resizeMode="contain"
+        />
+      </View>
+      <Text style={styles.jobTitle}>{jobTitle}</Text>
+      <Text style={styles.companyName}>{companyName}</Text>
+      <Text style={styles.locationName}>{location}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
